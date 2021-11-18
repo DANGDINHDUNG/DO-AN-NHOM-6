492d840971fa7212137b0581a741183678beb896
DANGDINHDUNG <91401719+DANGDINHDUNG@users.noreply.github.com>
2021-11-18 23:07:21 +0700

    Update KhachHang.Designer.cs

--- code/KhachHang.Designer.cs
+++ code/KhachHang.Designer.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace form
 {
     partial class KhachHang
@@ -44,6 +44,7 @@ namespace form
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -237,18 +238,18 @@ namespace form
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.button1.Location = new System.Drawing.Point(941, 354);
+            this.button1.Location = new System.Drawing.Point(936, 355);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 52);
+            this.button1.Size = new System.Drawing.Size(370, 52);
             this.button1.TabIndex = 42;
-            this.button1.Text = "Thêm/ Cập nhật";
+            this.button1.Text = "Thêm/ Cập nhật thông tin khách hàng";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.button2.Location = new System.Drawing.Point(1166, 354);
+            this.button2.Location = new System.Drawing.Point(1174, 437);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(132, 52);
             this.button2.TabIndex = 43;
@@ -256,11 +257,23 @@ namespace form
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button3.Location = new System.Drawing.Point(936, 437);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 52);
+            this.button3.TabIndex = 44;
+            this.button3.Text = "Đổi phòng";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // KhachHang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1361, 612);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
@@ -296,5 +309,6 @@ namespace form
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
