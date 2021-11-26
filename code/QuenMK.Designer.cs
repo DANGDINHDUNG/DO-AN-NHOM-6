5cdf91a2096858a5cc7104901bb820da7e03edf3
GHuy <92932665+NHGHuy@users.noreply.github.com>
2021-11-26 12:01:21 +0700

    Add files via upload

---
+++ code/QuenMK.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace DOAN
+{
+    partial class QuenMK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tdnBx = new System.Windows.Forms.TextBox();
+            this.cmndBx = new System.Windows.Forms.TextBox();
+            this.mkmBx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tdnBx
+            // 
+            this.tdnBx.Location = new System.Drawing.Point(167, 46);
+            this.tdnBx.Name = "tdnBx";
+            this.tdnBx.Size = new System.Drawing.Size(226, 20);
+            this.tdnBx.TabIndex = 0;
+            // 
+            // cmndBx
+            // 
+            this.cmndBx.Location = new System.Drawing.Point(167, 131);
+            this.cmndBx.Name = "cmndBx";
+            this.cmndBx.Size = new System.Drawing.Size(226, 20);
+            this.cmndBx.TabIndex = 0;
+            // 
+            // mkmBx
+            // 
+            this.mkmBx.Location = new System.Drawing.Point(167, 239);
+            this.mkmBx.Name = "mkmBx";
+            this.mkmBx.Size = new System.Drawing.Size(226, 20);
+            this.mkmBx.TabIndex = 0;
+            this.mkmBx.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tên đăng nhập";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật khẩu mới";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "CMND/CCCD";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(414, 240);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Xác nhận";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // QuenMK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 346);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mkmBx);
+            this.Controls.Add(this.cmndBx);
+            this.Controls.Add(this.tdnBx);
+            this.Name = "QuenMK";
+            this.Text = "QuenMK";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tdnBx;
+        private System.Windows.Forms.TextBox cmndBx;
+        private System.Windows.Forms.TextBox mkmBx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ code/QuenMK.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DOAN
+{
+    public partial class QuenMK : Form
+    {
+        public QuenMK()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            mkmBx.UseSystemPasswordChar = !mkmBx.UseSystemPasswordChar;
+        }
+
+        private void check()
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-K8QQEUE;Initial Catalog=QL;Integrated Security=True");
+            connection.Open();
+            string query = "select * from NHANVIEN where TENTK='" + tdnBx.Text + "'and CCCD_CMND='" + cmndBx.Text + "'";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.Read() == true)
+            {
+                change_pass();
+            }
+            else
+            {
+                MessageBox.Show("Không tồn tại tài khoản", "Thống báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void change_pass()
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-K8QQEUE;Initial Catalog=QLKS;Integrated Security=True");
+            connection.Open();
+            string query = "update NHANVIEN set MATKHAU='" + mkmBx.Text + "'where TENTK='" + tdnBx.Text + "'";
+            SqlCommand command = new SqlCommand(query, connection);
+            int c = command.ExecuteNonQuery();
+            MessageBox.Show("Bạn đã đổi mật khẩu thành công", "Thông báo", MessageBoxButtons.OK);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            check();
+        }
+    }
+}
